a859d4402f75b5908f771a9335a14a6cdedfcdf5
SHU023 <shu.rarara21@gmail.com>
2019-09-17 15:03:03 +0900

    proatice pyvisa1.0

--- 20190917.py
+++ 20190917.py
@@ -2,8 +2,14 @@
 #created on 2019/9/17　@auther SHU
 #python visa 使い方
 
-#モジュールをimport 
-import visa　
+#モジュールを入れる
 #visaを使う前の呪文
-rm = visa.ResourceManager()
-# 
+#接続機器全て表示
+#使用デバイスの指定writeはデバイスに命令、readは読み込み、queryは両方である
+#（）の中身はSCPIプログラム
+import visa
+rm =visa.ResourceManager()
+rm.list_resources()
+inst=rm.open_resource('USB0::0x2A8D::0x0201::MY57700883::0::INSTR')
+print(inst.query("*IDN?"))
+
